0092c8beb8b950dd5253af5196fbf85fe03cc890
kimuzinuan <a01020532509@gmail.com>
2023-03-23 22:00:20 +0900

    codes

---
+++ 0323웹마켓상품메소드.java
@@ -0,0 +1,77 @@
+package dto;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+	
+	private static final long serialVersionUID = -4274700572038677000L;
+	
+      private String productId;
+      private String pname;
+	  private Integer unitPrice;
+	  private String description;
+	  private String manufacturer;
+	  private String category;
+	  private long unitsInstock;
+	  private String condition;
+	  
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public Integer getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(Integer unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public long getUnitsInstock() {
+		return unitsInstock;
+	}
+	public void setUnitsInstock(long unitsInstock) {
+		this.unitsInstock = unitsInstock;
+	}
+	public String getCondition() {
+		return condition;
+	}
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+	  public Product() {
+		  super();
+	  }
+	  
+     
+
+public Product(String productId, String pname, Integer unitPrice) {
+	this.productId = productId;
+	this.pname = pname;
+	this.unitPrice = unitPrice;
+}
+}
