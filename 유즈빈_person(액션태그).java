7ed84ac7792649761aa8007be0481e325e7aaad5
kimuzinuan <a01020532509@gmail.com>
2023-03-30 22:54:57 +0900

    codes

---
+++ 유즈빈_person(액션태그).java
@@ -0,0 +1,24 @@
+package ch04.com.dao;
+
+public class Person {
+
+	private int id = 201801544;
+	private String name = "김진원";
+	
+	public Person() {
+		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
