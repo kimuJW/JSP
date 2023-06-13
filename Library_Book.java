a40f09e5f4cdf3096479485279cdee47e8618c65
kimuJW <a01020532509@gmail.com>
2023-06-14 01:22:07 +0900

    codes

---
+++ Library_Book.java
@@ -0,0 +1,59 @@
+package function;
+
+public class Book {
+
+    private String Bcode;
+    private String Bname;
+    private String Author;
+    private String Publisher;
+    private String Place;
+    private String Callsign;
+
+    public String getBcode() {
+        return Bcode;
+    }
+
+    public void setBcode(String Bcode) {
+        this.Bcode = Bcode;
+    }
+
+    public String getBname() {
+        return Bname;
+    }
+
+    public void setBname(String Bname) {
+        this.Bname = Bname;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String Author) {
+        this.Author = Author;
+    }
+
+    public String getPublisher() {
+        return Publisher;
+    }
+
+    public void setPublisher(String Publisher) {
+        this.Publisher = Publisher;
+    }
+
+    public String getPlace() {
+        return Place;
+    }
+
+    public void setPlace(String Place) {
+        this.Place = Place;
+    }
+
+    public String getCallsign() {
+        return Callsign;
+    }
+
+    public void setCallsign(String Callsign) {
+        this.Callsign = Callsign;
+    }
+}
