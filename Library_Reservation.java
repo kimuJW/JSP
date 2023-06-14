b0342176d12acb3c401d483bdaf55d3a08eaf581
kimuJW <a01020532509@gmail.com>
2023-06-15 01:32:05 +0900

    codes

---
+++ Library_Reservation.java
@@ -0,0 +1,42 @@
+package function;
+
+public class Reservation {
+
+    private String Rcode;
+    private String Name;
+    private String ID;
+    private String Bcode;
+    private String Bname;
+	public String getRcode() {
+		return Rcode;
+	}
+	public void setRcode(String rcode) {
+		Rcode = rcode;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getBcode() {
+		return Bcode;
+	}
+	public void setBcode(String bcode) {
+		Bcode = bcode;
+	}
+	public String getBname() {
+		return Bname;
+	}
+	public void setBname(String bname) {
+		Bname = bname;
+	}
+
+   
+}
